bbb6a86d0ca80f827f7da97b6eb76a09abdca351
jemil chovatiya <jemilchovatiya18@gmail.com>
2026-03-27 15:02:31 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,12 @@ app.use(express.urlencoded({extended:true, linit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// import router
+import userRouter from './routes/user.route.js'
+
+// routerr declaration
+app.use("/api/v1/users", userRouter)
+
+//http:localhost:8000/api/v1/users/register
+
 export { app } 

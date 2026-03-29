ad25b40f6d9960aecf28c769ef7e09d397564eec
jemil chovatiya <jemilchovatiya18@gmail.com>
2026-03-29 14:03:07 +0530

    add refreshaccesstoken

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,12 +1,12 @@
-import { ApiError } from "../utils/ApiError";
-import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
-import { User } from "../models/user.model";
+import { User } from "../models/user.model.js";
 
 
 export const verifyJWT = asyncHandler(async(req,res,next) => {
     try {
-        const token = req.cookie?.accessToken || req.header("Authorization")?.replace("Bearer", "")
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
     
         if(!token){
             throw new ApiError(401, "Unauthorized request")
@@ -24,5 +24,7 @@ export const verifyJWT = asyncHandler(async(req,res,next) => {
         next()
     } catch (error) {
         throw new ApiError(401, error?.massage || "Invalid access token")
+        // console.log("REAL ERROR:", error);  // 👈 ye add karo
+        // throw error;   
     }
 })
